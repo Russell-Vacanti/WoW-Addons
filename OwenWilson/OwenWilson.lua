b7973405e18b0ee6eb742991f19c034b6afd4967
Russell Vacanti <rvacanti@gmail.com>
2018-08-24 13:18:32 -0500

    random sounds play now

--- OwenWilson/OwenWilson.lua
+++ OwenWilson/OwenWilson.lua
@@ -1,40 +1,26 @@
+local WOWsounds = {}
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowa.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowb.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowc.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowd.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowe.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowf.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowg.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowh.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowi.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowj.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\wowk.mp3'
+WOWsounds[#WOWsounds+1] = 'Interface\\AddOns\\OwenWilson\\sounds\\Kachow.mp3'
+local soundLength = #WOWsounds
+
 local lootFrame = CreateFrame("Frame")
-
-local sounds = {
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowa.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowb.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowc.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowd.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowe.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowf.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowg.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowh.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowi.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowj.mp3',
-  'Interface\\AddOns\\OwenWilson\\Sounds\\wowk.mp3'
-}
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowa.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowb.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowc.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowd.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowe.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowf.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowg.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowh.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowi.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowj.mp3')
-table.insert(sounds, 'Interface\\AddOns\\OwenWilson\\Sounds\\wowk.mp3')
-
-local kachow = 'Interface\\AddOns\\OwenWilson\\Sounds\\Kachow.mp3'
-local soundLength = 0
-message('Owen Wilson Wow Addon is loaded.')
-math.randomseed(os.time())
-for index in pairs(sounds) do
-    soundLength = soundLength + 1
-end
 
 lootFrame:RegisterEvent("LOOT_OPENED")
 
+
+message('Owen Wilson loaded ' .. soundLength .. ' looting sounds.')
+
 lootFrame:SetScript("OnEvent",function(self, event, ...)
-  PlaySoundFile(sounds[math.random(table.getn(sounds))] , "Master")
+	local g = math.random(soundLength)
+	PlaySoundFile(WOWsounds[g], "Master")
 end)
